0ea6c7d592ce24461a2f85aafca3013d4a22e518
JohnOpioid <evgeniy.odin@gmail.com>
2025-10-12 16:30:17 +0300

    feat: добавлены границы к кнопкам сохранения и обновления
    - Добавлен border-2 для кнопки 'Сохранить приложение' (синий border)
    - Добавлен border-2 для кнопки 'Обновить приложение' (оранжевый border)
    - Поддержка темной темы с соответствующими цветами границ
    - Кнопки стали более заметными и визуально выделенными

--- components/CacheManager.vue
+++ components/CacheManager.vue
@@ -6,7 +6,7 @@
       @click="precacheSite"
       color="blue"
       size="xl"
-      class="shadow-lg"
+      class="shadow-lg border-2 border-blue-300 dark:border-blue-600"
       :loading="isCaching"
     >
       <Icon name="heroicons:device-phone-mobile" class="w-5 h-5 mr-2" />
@@ -35,7 +35,7 @@
       @click="updateApp"
       color="orange"
       size="xl"
-      class="shadow-lg animate-pulse"
+      class="shadow-lg animate-pulse border-2 border-orange-300 dark:border-orange-600"
     >
       <Icon name="heroicons:arrow-path" class="w-5 h-5 mr-2" />
       Обновить приложение
